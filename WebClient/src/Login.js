dfc578b213484d7bd8192fac1c09513032862f36
AlanKhamo <alankhamo@gmail.com>
2023-05-11 15:25:16 +0200

    update login routing

--- WebClient/src/Login.js
+++ WebClient/src/Login.js
@@ -1,23 +1,23 @@
 
 import React, { Component, useState } from 'react';
-import {useNavigate, redirect} from 'react-router-dom';
+import { useNavigate, redirect } from 'react-router-dom';
 import './Login.css';
 
 export default function Login() {
 
 
-  return(
+  return (
 
     <div className="login-wrapper">
       <h1>Please Log In</h1>
       <form onSubmit={Sendlogin}>
         <label>
           <p>Username</p>
-          <input type="text"  id="Email"/> 
+          <input type="text" id="Email" />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" id="Pass"/>
+          <input type="password" id="Pass" />
         </label>
         <div>
           <button type="submit">Submit</button>
@@ -26,46 +26,46 @@ export default function Login() {
     </div>
   )
 }
-  function Sendlogin(event) {
-    event.preventDefault()
-    console.log(event.target[0].value)
-    console.log(event.target[1].value)
-    const  payload = {
-      email: event.target[0].value,
-      password: event.target[1].value
-    }
-    
+function Sendlogin(event) {
+  event.preventDefault()
+  console.log(event.target[0].value)
+  console.log(event.target[1].value)
+  const payload = {
+    email: event.target[0].value,
+    password: event.target[1].value
+  }
 
-    fetch('https://localhost:7181/api/Account/Login', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
+
+  fetch('https://localhost:7181/api/Account/Login', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(payload)
   })
-  .then(res => res.json())
-  .then((token)=> {
-    console.log(token.jwt);
-    localStorage.setItem("token", token.jwt);
-    let RoleExtracted = parseToJwt(token.jwt);
-    console.log(RoleExtracted);
-    let role =
-    RoleExtracted["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
-    localStorage.setItem("role", role);
-    let ModelId = RoleExtracted["ModelId"];
-    localStorage.setItem("ModelId", ModelId);
-    window.location.href = "/CreateJob";
-  },
-  (error) => {
-      console.log(error);
-      
-    }
-  
-  );
-  
-    
-  
+    .then(res => res.json())
+    .then((token) => {
+      console.log(token.jwt);
+      localStorage.setItem("token", token.jwt);
+      let RoleExtracted = parseToJwt(token.jwt);
+      console.log(RoleExtracted);
+      let role =
+        RoleExtracted["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
+      localStorage.setItem("role", role);
+      let ModelId = RoleExtracted["ModelId"];
+      localStorage.setItem("ModelId", ModelId);
+      window.location.href = "/SeeJob";
+    },
+      (error) => {
+        console.log(error);
+
+      }
+
+    );
+
+
+
 }
 
 function parseToJwt(token) {
@@ -74,11 +74,11 @@ function parseToJwt(token) {
   var jsonPayload = decodeURIComponent(
     atob(base64)
       .split('')
-      .map(function(c) {
+      .map(function (c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
       })
       .join('')
   );
-  
+
   return JSON.parse(window.atob(base64));
 }
